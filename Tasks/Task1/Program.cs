b6c69b1bfde413b17a179603a04ebd2b2f1a0190
andy <andyggggg>
2024-02-19 18:16:24 +0300

    add Task1 homework

--- Tasks/Task1/Program.cs
+++ Tasks/Task1/Program.cs
@@ -1,12 +1,14 @@
 ﻿// Задача 2: Задайте массив заполненный случайными трёхзначными числами. Напишите программу, которая
 //  покажет количество чётных чисел в массиве.
+Console.WriteLine("Введите желаемое колличество чисел в массиве");
 int number = Convert.ToInt32(Console.ReadLine());
-
+int count =0;
 int[] array = new  int[number];
     for(int i = 0; i < number; i++ ) {
         array[i] = new Random().Next(99,1000);
-
-    }
-    for(int i = 0; i < number; i++ ) {
-        Console.WriteLine(array[i] +"");
-    }
+        Console.Write(array[i] +" ");
+            if(array[i] % 2 == 0) {
+                count += 1;
+            }
+    }      
+    Console.WriteLine($"Колличество четных чисел в массиве равно {count}");

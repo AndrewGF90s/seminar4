502b68d679a698af6d9c4b356bb763f4781b288a
andy <andyggggg>
2024-02-19 17:54:19 +0300

    add Task

--- Tasks/Task1/Program.cs
+++ Tasks/Task1/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Задайте массив заполненный случайными трёхзначными числами. Напишите программу, которая
+//  покажет количество чётных чисел в массиве.
+int number = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new  int[number];
+    for(int i = 0; i < number; i++ ) {
+        array[i] = new Random().Next(99,1000);
+
+    }
+    for(int i = 0; i < number; i++ ) {
+        Console.WriteLine(array[i] +"");
+    }

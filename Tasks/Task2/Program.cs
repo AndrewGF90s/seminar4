cc2309b9fa34451c5d856c25b3c1b1316c5b6f53
andy <andyggggg>
2024-02-20 23:06:13 +0300

    add Homework2

--- Tasks/Task2/Program.cs
+++ Tasks/Task2/Program.cs
@@ -1,1 +1,17 @@
 ﻿
+// Задача 3: Напишите программу, которая перевернёт
+// одномерный массив (первый элемент станет
+// последним, второй – предпоследним и т.д.)
+// [1 3 5 6 7 8] => [8 7 6 5 3 1]
+int[] array = {1,3,5,6,7,8};
+int temp = 0;
+       for (int i = 0, j = array.Length -1; i < j ; i++, j--) {
+            temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+       }
+        for (int i = 0;i < array.Length; i++) {
+             Console.Write(array[i]+ " "); 
+        }
+                
+
---
+++ Tasks/Task3/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 1: Напишите программу, которая бесконечно запрашивает целые числа с консоли. Программа завершается при 
+// вводе символа ‘q’ или при вводе числа, сумма цифр которого чётная.
+int q = 8912;
+int leftDigit = number / 100 ; 
+int midleDigit = number %100 / 10;
+int rightDigit = number % 10;
+int sumNumber = leftDigit + midleDigit + rightDigit;
+ 
+
+ for (;;) {giveMenumber ();}
+ giveMenumber (); {
+
+    Console.WriteLine("Введите целое, трехзначное  число");
+int number = Convert.ToInt32(Console.ReadLine());
+ 
+    if (number > 99 && number <1000) {
+        if(number % 2 == 0 || number % 2 == 1 ) {
+       
+            if (number == q) {
+                break;
+            }
+            else if (sumNumber % 2 == 0) {
+                break;
+            }
+        }
+
+    } 
+
+    }
+    
+    Console.WriteLine($" {leftDigit}Введите {midleDigit}целое{rightDigit} число{sumNumber}");
+
+
+
